11c04480201cb6b9cae86e83c9ec57689371cb9d
Iam_Hey-k <60710389+harkeyhim@users.noreply.github.com>
2023-09-12 12:10:07 +0100

    Initial Commit

---
+++ HNG Task 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HNG Task 2
+//
+//  Created by Yusuf Akeem on 12/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
